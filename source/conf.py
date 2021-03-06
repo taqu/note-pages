0eb587a2d405e15f91a28edbfa069acf834e1ff7
Linux User <taqu@localhost.my.domain>
2021-03-06 23:46:07 +0900

    Add test pages

--- source/conf.py
+++ source/conf.py
@@ -31,7 +31,6 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        'sphinx.ext.mathjax', 'sphinx.ext.todo', 'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -55,11 +54,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'haiku'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-
--- source/index.rst
+++ source/index.rst
@@ -1,5 +1,5 @@
 .. note-pages documentation master file, created by
-   sphinx-quickstart on Sun Feb 28 04:47:58 2021.
+   sphinx-quickstart on Sat Mar  6 23:38:28 2021.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
